379ad4cc46222f07675e72952bf184e0ec17c83b
Abhishek More <abhimore74@gmail.com>
2023-11-04 16:40:35 -0500

    fixed tailwind

--- App.tsx
+++ App.tsx
@@ -4,9 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text className="hidden">
-        Open up App.js to start working on your app!
-      </Text>
+      <Text className="text-3xl">Open up App.js to start working app!</Text>
       <StatusBar style="auto" />
     </View>
   );
