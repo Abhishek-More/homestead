8971a533b7919c5d34f192215b2770192f91fe39
Abhishek More <abhimore74@gmail.com>
2023-11-05 01:46:22 -0500

    location

--- app/location.tsx
+++ app/location.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Platform, Text, View, StyleSheet } from "react-native";
+import MapView from "react-native-maps";
 
 import * as Location from "expo-location";
 
@@ -25,11 +26,33 @@ export default function App() {
     text = errorMsg;
   } else if (location) {
     text = JSON.stringify(location);
+    console.log(location);
   }
 
   return (
-    <View className="flex flex-row justify-center items-center">
-      <Text>{text}</Text>
+    <View style={styles.container}>
+      {!location && <Text>Loading...</Text>}
+      {location && (
+        <MapView
+          initialRegion={{
+            latitude: location?.coords?.latitude || 37.78825,
+            longitude: location?.coords?.longitude || -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          style={styles.map}
+        />
+      )}
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
